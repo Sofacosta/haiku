4bb6ea2c9455ab1a9df45c75fd7377f7bd3881b8
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-03 10:45:58 -0800

    Successfully failed first test
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,7 @@
+import Haiku from './../src/haiku.js';
+describe ('Haiku', () => {
+  test ('should create an object', () => {
+    const haiku = new Haiku('a', 'b', 'c');
+    expect(haiku).toEqual();
+  });
+});
