a21fcd0ded2156b74b86e8ef6ec6d18934258530
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-03 11:08:06 -0800

    Pass first test, create constructor successfully
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,6 +2,6 @@ import Haiku from './../src/haiku.js';
 describe ('Haiku', () => {
   test ('should create an object', () => {
     const haiku = new Haiku('a', 'b', 'c');
-    expect(haiku).toEqual();
+    expect(haiku.line1).toEqual('a');
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,5 +1,10 @@
-export class Haiku {
-  constructor(line1, line2, line3){
+// class Haiku {
+//   constructor(line1, line2, line3){
+//   }
+// };
 
-  }
-};
+export default function Haiku(line1, line2, line3){
+  this.line1 = line1;
+  this.line2 = line2;
+  this.line3 = line3;
+}
